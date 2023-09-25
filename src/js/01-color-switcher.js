3efd7f3bac91cbf1ab3d3b4050e73a475c42bf06
lirikape <lirikape@gmail.com>
2023-09-25 17:32:40 +0300

    hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  }
+  
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+  const body = document.body;
+  
+  let intervalId = null;
+  
+  startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 700);
+  });
+  
+  stopButton.addEventListener('click', () => {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    
+    clearInterval(intervalId);
+  });
